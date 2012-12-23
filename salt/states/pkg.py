layer3switch/salt

commit 0b5ce09f759e4339600d59321984f778eeb1c723

Salt State pkg.installed enforce package versions
Previously if there was a version installed (any version) it would return as "enforced" this patch makes sure that salt state enforces the correct version.

salt/states/pkg.py
@@ -40,7 +40,7 @@ def installed(
         sources=None,
         **kwargs):
     '''
-    Verify that the package is installed, and that it is the correct version.
+    Verify that the package is installed, and that it is the correct version (if specified).
 
     name
         The name of the package to be installed. This parameter is ignored if
@@ -144,6 +144,14 @@ def installed(
                     'result': True,
                     'comment': ('Package {0} is already installed and is the '
                                 'correct version').format(name)}
+        # if a version wasn't specified, we are done
+        elif version is None:
+            # The package is installed
+            return {'name': name,
+                    'changes': {},
+                    'result': True,
+                    'comment': 'Package {0} is already installed'.format(name)}
+
 
     if __opts__['test']:
         if len(targets) > 1:

commit 8ef616c4a1bbe6ba9fd593622ae880b04740a7c3

Fix pkg state regression
acd949c and 0b5ce09 broke the pkg state, such that for single pkg
installs (i.e. no "pkgs" or "sources"), if the version arg is not
provided, a package will always be interpreted by the pkg state as being
installed, and the package will never be installed. This commit fixes
that regression.

salt/states/pkg.py
@@ -144,8 +144,9 @@ def installed(
                     'result': True,
                     'comment': ('Package {0} is already installed and is the '
                                 'correct version').format(name)}
-        # if a version wasn't specified, we are done
-        elif version is None:
+
+        # if cver is not an empty string, the package is already installed
+        elif cver:
             # The package is installed
             return {'name': name,
                     'changes': {},