layer3switch/salt

commit 0c9bcab35507f4307126322a86fdcbb6bccd425b

Worthless booleans removal

salt/states/mount.py
@@ -90,12 +90,10 @@ def mounted(name,
             # Failed to (re)mount, the state has failed!
             ret['comment'] = out
             ret['result'] = False
-            mounted = False
         elif out is True:
             # (Re)mount worked!
             ret['comment'] = 'Target was successfully mounted'
             ret['changes']['mount'] = True
-            mounted = True
     else:
         ret['comment'] = 'Target was already mounted'
 
@@ -108,8 +106,7 @@ def mounted(name,
                                   'be set to be made persistent').format(name)
                 return ret
 
-        if mounted:
-            # Make sure the entry is in the fstab
+        if ret['changes']['mount']:
             out = __salt__['mount.set_fstab'](name,
                                               device,
                                               fstype,

commit 2d9517f46ff7b5f7cd23cce9c7914a33c40a6c59

Fix traceback in mount.mounted state
This fixes a traceback introduced in 0c9bcab3.

salt/states/mount.py
@@ -112,7 +112,7 @@ def mounted(name,
                                   'be set to be made persistent').format(name)
                 return ret
 
-        if ret['changes']['mount']:
+        if ret['changes'].get('mount'):
             out = __salt__['mount.set_fstab'](name,
                                               device,
                                               fstype,