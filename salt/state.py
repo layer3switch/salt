layer3switch/salt

commit fcfe3a99f4df3df2aa84ee5e4c7cba9b006a82ad

Since salt required python >= 2.6 we can use the `getargspec` named tuple.

salt/state.py
@@ -620,14 +620,18 @@ class State(object):
             aspec = salt.utils.get_function_argspec(self.states[full])
             arglen = 0
             deflen = 0
-            if isinstance(aspec[0], list):
-                arglen = len(aspec[0])
-            if isinstance(aspec[3], tuple):
-                deflen = len(aspec[3])
+            if isinstance(aspec.args, list):
+                arglen = len(aspec.args)
+            if isinstance(aspec.defaults, tuple):
+                deflen = len(aspec.defaults)
             for ind in range(arglen - deflen):
-                if aspec[0][ind] not in data:
-                    errors.append('Missing parameter ' + aspec[0][ind]
-                                + ' for state ' + full)
+                if aspec.args[ind] not in data:
+                    errors.append(
+                        'Missing parameter {0} for state {1}'.format(
+                            aspec.args[ind],
+                            full
+                        )
+                    )
         # If this chunk has a recursive require, then it will cause a
         # recursive loop when executing, check for it
         reqdec = ''
@@ -841,27 +845,27 @@ class State(object):
         aspec = salt.utils.get_function_argspec(self.states[ret['full']])
         arglen = 0
         deflen = 0
-        if isinstance(aspec[0], list):
-            arglen = len(aspec[0])
-        if isinstance(aspec[3], tuple):
-            deflen = len(aspec[3])
-        if aspec[2]:
-            # This state accepts kwargs
+        if isinstance(aspec.args, list):
+            arglen = len(aspec.args)
+        if isinstance(aspec.defaults, tuple):
+            deflen = len(aspec.defaults)
+        if aspec.keywords:
+            # This state accepts **kwargs
             ret['kwargs'] = {}
             for key in data:
                 # Passing kwargs the conflict with args == stack trace
-                if key in aspec[0]:
+                if key in aspec.keywords:
                     continue
                 ret['kwargs'][key] = data[key]
         kwargs = {}
         for ind in range(arglen - 1, 0, -1):
             minus = arglen - ind
             if deflen - minus > -1:
-                kwargs[aspec[0][ind]] = aspec[3][-minus]
+                kwargs[aspec.args[ind]] = aspec.defaults[-minus]
         for arg in kwargs:
             if arg in data:
                 kwargs[arg] = data[arg]
-        for arg in aspec[0]:
+        for arg in aspec.args:
             if arg in kwargs:
                 ret['args'].append(kwargs[arg])
             else:

salt/states/module.py
@@ -70,15 +70,15 @@ def run(name, **kwargs):
 
     arglen = 0
     deflen = 0
-    if isinstance(aspec[0], list):
-        arglen = len(aspec[0])
-    if isinstance(aspec[3], tuple):
-        deflen = len(aspec[3])
+    if isinstance(aspec.args, list):
+        arglen = len(aspec.args)
+    if isinstance(aspec.defaults, tuple):
+        deflen = len(aspec.defaults)
     # Match up the defaults with the respective args
     for ind in range(arglen - 1, -1, -1):
         minus = arglen - ind
         if deflen - minus > -1:
-            defaults[aspec[0][ind]] = aspec[3][-minus]
+            defaults[aspec.args[ind]] = aspec.defaults[-minus]
     # overwrite passed default kwargs
     for arg in defaults:
         if arg == 'name':
@@ -90,7 +90,7 @@ def run(name, **kwargs):
         if arg in kwargs:
             defaults[arg] = kwargs.pop(arg)
     missing = set()
-    for arg in aspec[0]:
+    for arg in aspec.args:
         if arg == 'name':
             rarg = 'm_name'
         elif arg == 'fun':
@@ -112,29 +112,29 @@ def run(name, **kwargs):
         ret['result'] = False
         return ret
 
-    if aspec[1] and aspec[1] in kwargs:
-        varargs = kwargs.pop(aspec[1])
+    if aspec.varargs and aspec.varargs in kwargs:
+        varargs = kwargs.pop(aspec.varargs)
 
         if not isinstance(varargs, list):
             msg = "'{0}' must be a list."
-            ret['comment'] = msg.format(aspec[1])
+            ret['comment'] = msg.format(aspec.varargs)
             ret['result'] = False
             return ret
 
         args.extend(varargs)
 
     nkwargs = {}
-    if aspec[2] and aspec[2] in kwargs:
-        nkwargs = kwargs.pop(aspec[2])
+    if aspec.keywords and aspec.keywords in kwargs:
+        nkwargs = kwargs.pop(aspec.keywords)
 
         if not isinstance(nkwargs, dict):
             msg = "'{0}' must be a dict."
-            ret['comment'] = msg.format(aspec[2])
+            ret['comment'] = msg.format(aspec.keywords)
             ret['result'] = False
             return ret
 
     try:
-        if aspec[2]:
+        if aspec.keywords:
             mret = __salt__[name](*args, **nkwargs)
         else:
             mret = __salt__[name](*args)

salt/utils/__init__.py
@@ -716,15 +716,15 @@ def arg_lookup(fun):
     aspec = get_function_argspec(fun)
     arglen = 0
     deflen = 0
-    if isinstance(aspec[0], list):
-        arglen = len(aspec[0])
-    if isinstance(aspec[3], tuple):
-        deflen = len(aspec[3])
+    if isinstance(aspec.args, list):
+        arglen = len(aspec.args)
+    if isinstance(aspec.defaults, tuple):
+        deflen = len(aspec.defaults)
     for ind in range(arglen - 1, 0, -1):
         minus = arglen - ind
         if deflen - minus > -1:
-            ret['kwargs'][aspec[0][ind]] = aspec[3][-minus]
-    for arg in aspec[0]:
+            ret['kwargs'][aspec.args[ind]] = aspec.defaults[-minus]
+    for arg in aspec.args:
         if arg in ret:
             continue
         else:

commit 5f2e301150dca3c74dac6d6a40cc86e3f5f660c9

Fix bug introduced in fcfe3a9 when changing argspec to use the named tuple

salt/state.py
@@ -854,7 +854,7 @@ class State(object):
             ret['kwargs'] = {}
             for key in data:
                 # Passing kwargs the conflict with args == stack trace
-                if key in aspec.keywords:
+                if key in aspec.args:
                     continue
                 ret['kwargs'][key] = data[key]
         kwargs = {}