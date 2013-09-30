layer3switch/salt

commit d749e5ad4f7e092d9679f14026e07211274cdf3f

Always wrap salt-call return dict
salt.cli.caller.Caller.run() was checking if the return data was a dict,
and wrapping it in an outer dict like so: {'local': ret['return']} if
the data was not a dict, to ensure that the return data is in the right
format for the different outputters.

The problem with this is that there are many execution functions that
already return a dict, such as network.interfaces, grains.items,
pillar.items, etc. These will not be wrapped. This results in a
traceback when running grains.items via salt-call, since the grains
outputter expects the return data to be a dict of dicts, with the
top-level keys being the minion id and the 2nd level being the minion's
grains dict.

This commit removes the isinstance check and simply always wraps the
return data in the 'local' dict.

salt/cli/caller.py
@@ -134,14 +134,8 @@ class Caller(object):
         Execute the salt call logic
         '''
         ret = self.call()
-        out = ret['return']
-        # If the type of return is not a dict we wrap the return data
-        # This will ensure that --local and local functions will return the
-        # same data structure as publish commands.
-        if not isinstance(ret['return'], dict):
-            out = {'local': ret['return']}
         salt.output.display_output(
-                out,
+                {'local': ret.get('return', {})},
                 ret.get('out', 'nested'),
                 self.opts)
         if self.opts.get('retcode_passthrough', False):

commit 3bb7a5ffa8217b91f94b394cd5803f5311cf9d63

Fix regression in salt-call
This fixes a regression that I inadvertently caused in d749e5a by
always wrapping the return data in a dict. An extra step here in the
Caller class was duplicating this action, causing a traceback when
attempting to increment the result counts for the highstate outputter.

salt/cli/caller.py
@@ -98,8 +98,6 @@ class Caller(object):
             oput = self.minion.functions[fun].__outputter__
             if isinstance(oput, string_types):
                 ret['out'] = oput
-            if oput == 'highstate':
-                ret['return'] = {'local': ret['return']}
         if self.opts.get('return', ''):
             ret['id'] = self.opts['id']
             ret['fun'] = fun