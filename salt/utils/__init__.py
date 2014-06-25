layer3switch/salt

commit 428bd43af471aa79cd3f5a1f8b0c77d78a07ce38

Allow traversing dicts nested with lists when matching

salt/utils/__init__.py
@@ -1263,12 +1263,29 @@ def traverse_dict_and_list(data, key, default, delim=':'):
     {'foo':{'bar':['baz']}} , if data like {'foo':{'bar':{'0':'baz'}}}
     then return data['foo']['bar']['0']
     '''
-    try:
-        for each in key.split(delim):
-            data = data[int(each)] if isinstance(data, list) else data[each]
-    except (KeyError, IndexError, TypeError, ValueError):
-        # Encountered a non-indexable value in the middle of traversing
-        return default
+    for each in key.split(delim):
+        if isinstance(data, list):
+            try:
+                idx = int(each)
+            except ValueError:
+                # Index was not numeric, lets look at any embedded dicts
+                for embedded in (x for x in data if isinstance(x, dict)):
+                    try:
+                        data = embedded[each]
+                    except KeyError:
+                        pass
+                # No embedded dicts matched, return the default
+                return default
+            else:
+                try:
+                    data = data[idx]
+                except IndexError:
+                    return default
+        else:
+            try:
+                data = data[each]
+            except KeyError:
+                return default
     return data
 
 

commit 1268ee1c37cba54fd9f3c74e66cde7d7efcaea64

Fix embedded data structure matches
This fixes 2 issues introduced in 428bd43:

1. When traversing too far into a multi-level dict and a bare string is
   reached, trying to index it with the next dict level in the path
   expression results in an uncaught TypeError (caught by unit tests).

2. Continuing the match once a match is found in a dict embedded in a
   list fails (not caught by unit tests, new test(s) forthcoming).

salt/utils/__init__.py
@@ -1268,14 +1268,18 @@ def traverse_dict_and_list(data, key, default, delim=':'):
             try:
                 idx = int(each)
             except ValueError:
+                embed_match = False
                 # Index was not numeric, lets look at any embedded dicts
                 for embedded in (x for x in data if isinstance(x, dict)):
                     try:
                         data = embedded[each]
+                        embed_match = True
+                        break
                     except KeyError:
                         pass
-                # No embedded dicts matched, return the default
-                return default
+                if not embed_match:
+                    # No embedded dicts matched, return the default
+                    return default
             else:
                 try:
                     data = data[idx]
@@ -1284,7 +1288,7 @@ def traverse_dict_and_list(data, key, default, delim=':'):
         else:
             try:
                 data = data[each]
-            except KeyError:
+            except (KeyError, TypeError):
                 return default
     return data
 