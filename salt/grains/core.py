layer3switch/salt

commit 885e00ba54ac5b9ca4e8eeded7393419e69df730

Only remove the word linux from distroname when its not part of the name
This will now still replace "CentOS Linux" with "CentOS" while leaving "CloudLinux" unmodified.
Fixes #28951

salt/grains/core.py
@@ -879,7 +879,7 @@ def id_():
     '''
     return {'id': __opts__.get('id', '')}
 
-_REPLACE_LINUX_RE = re.compile(r'linux', re.IGNORECASE)
+_REPLACE_LINUX_RE = re.compile(r'\Wlinux', re.IGNORECASE)
 
 # This maps (at most) the first ten characters (no spaces, lowercased) of
 # 'osfullname' to the 'os' grain that Salt traditionally uses.

commit 242e3ddc01b59dec4fd65af33d56ca14ac3800fa

Fix debian grains setup
This was broken in 885e00b because the updated regex now removes the
slash in 'Debian GNU/Linux'.

This issue did not make it into an official release, it exists only in
the head of the 2015.8 branch.

salt/grains/core.py
@@ -908,7 +908,7 @@ def id_():
     '''
     return {'id': __opts__.get('id', '')}
 
-_REPLACE_LINUX_RE = re.compile(r'\Wlinux', re.IGNORECASE)
+_REPLACE_LINUX_RE = re.compile(r'\W(?:gnu/)?linux', re.IGNORECASE)
 
 # This maps (at most) the first ten characters (no spaces, lowercased) of
 # 'osfullname' to the 'os' grain that Salt traditionally uses.
@@ -920,8 +920,7 @@ _OS_NAME_MAP = {
     'archarm': 'Arch ARM',
     'arch': 'Arch',
     'debian': 'Debian',
-    'debiangnu/': 'Debian',
-    'raspbiangn': 'Raspbian',
+    'raspbian': 'Raspbian',
     'fedoraremi': 'Fedora',
     'amazonami': 'Amazon',
     'alt': 'ALT',