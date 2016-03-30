layer3switch/salt

commit e61ac75d6f04e694bdb6440fffe5634fcbba7c8b

Fix filter_by passing incorrect parameters to match functions

salt/modules/match.py
@@ -333,7 +333,7 @@ def filter_by(lookup, expr_form='compound', minion_id=None):
     for key in lookup:
         if minion_id and expr_funcs[expr_form](key, minion_id):
             return lookup[key]
-        elif expr_funcs[expr_form](key, minion_id):
+        elif expr_funcs[expr_form](key):
             return lookup[key]
 
     return None

commit 83448dcebfafe848414dc0108504a489d5a60a86

Bugfix: match.filter_by minion_id regression fix
There is a regression since e61ac75d6f04e694bdb6440fffe5634fcbba7c8b.
Even when minion_id is provided it tries to match against id from opts.

salt/modules/match.py
@@ -331,9 +331,8 @@ def filter_by(lookup, expr_form='compound', minion_id=None):
         predicate=inspect.isfunction))
 
     for key in lookup:
-        if minion_id and expr_funcs[expr_form](key, minion_id):
-            return lookup[key]
-        elif expr_funcs[expr_form](key):
+        params = (key, minion_id) if minion_id else (key, )
+        if expr_funcs[expr_form](*params):
             return lookup[key]
 
     return None