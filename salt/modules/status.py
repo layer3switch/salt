layer3switch/salt

commit 9042aa61acc885a82f440318de82792ac36d1a3c

Fix bug in /proc/net/dev parsing

salt/modules/status.py
@@ -317,6 +317,10 @@ def netdev():
         if line.find(':') < 0:
             continue
         comps = line.split()
+        # Fix lines like eth0:9999..'
+        comps[0] = line.split(':')[0].strip()
+        #Support lines both like eth0:999 and eth0: 9999
+        comps[1] = line.split(':')[1].strip().split()[0]
         ret[comps[0]] = {'iface': comps[0],
                          'rx_bytes': _number(comps[1]),
                          'rx_compressed': _number(comps[7]),

commit 71cfcbe4892cd71367c06f34c121432c692f8dc7

fix bug in status.netdev
This was introduced in 9042aa61acc885a82f440318de82792ac36d1a3c
and then later fixed to be an insert here
703f2b4dc6c40e90f90c2cb29d25d2f555de2aa3, but all the comps values were never
shifted up 1 to take into account that the list was now one entry longer

salt/modules/status.py
@@ -1199,22 +1199,22 @@ def netdev():
                 # Support lines both like eth0:999 and eth0: 9999
                 comps.insert(1, line.split(':')[1].strip().split()[0])
                 ret[comps[0]] = {'iface': comps[0],
-                                 'rx_bytes': _number(comps[1]),
-                                 'rx_compressed': _number(comps[7]),
-                                 'rx_drop': _number(comps[4]),
-                                 'rx_errs': _number(comps[3]),
-                                 'rx_fifo': _number(comps[5]),
-                                 'rx_frame': _number(comps[6]),
-                                 'rx_multicast': _number(comps[8]),
-                                 'rx_packets': _number(comps[2]),
-                                 'tx_bytes': _number(comps[9]),
-                                 'tx_carrier': _number(comps[15]),
-                                 'tx_colls': _number(comps[14]),
-                                 'tx_compressed': _number(comps[16]),
-                                 'tx_drop': _number(comps[12]),
-                                 'tx_errs': _number(comps[11]),
-                                 'tx_fifo': _number(comps[13]),
-                                 'tx_packets': _number(comps[10])}
+                                 'rx_bytes': _number(comps[2]),
+                                 'rx_compressed': _number(comps[8]),
+                                 'rx_drop': _number(comps[5]),
+                                 'rx_errs': _number(comps[4]),
+                                 'rx_fifo': _number(comps[6]),
+                                 'rx_frame': _number(comps[7]),
+                                 'rx_multicast': _number(comps[9]),
+                                 'rx_packets': _number(comps[3]),
+                                 'tx_bytes': _number(comps[10]),
+                                 'tx_carrier': _number(comps[16]),
+                                 'tx_colls': _number(comps[15]),
+                                 'tx_compressed': _number(comps[17]),
+                                 'tx_drop': _number(comps[13]),
+                                 'tx_errs': _number(comps[12]),
+                                 'tx_fifo': _number(comps[14]),
+                                 'tx_packets': _number(comps[11])}
         return ret
 
     def freebsd_netdev():